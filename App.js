6368ed361688cd6676eee352918401db18abf418
Ercan <70813122+234ercan@users.noreply.github.com>
2022-01-17 10:01:01 +0000

    Add files via upload

--- App.js
+++ App.js
@@ -1,39 +1,23 @@
-import React,{Component} from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+//RNCS
+import React, { Component } from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+import Login from './src/pages/Login'
 
-export default class App extends Component {  
-state={counter:0}
-_onPressIncrease= ()=>{this.setState({counter:++this.state.counter})}
-_onPressDecrease= ()=>{this.setState({counter:--this.state.counter})}
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-      <Text style={styles.title}>{this.state.counter}</Text>
-      <View style={styles.buttonField}>
-      <Button title="-" onPress={this._onPressDecrease}/>
-      <Button title="+" onPress={this._onPressIncrease}/>
+        <Text> textInComponent </Text>
       </View>
-    </View>
-    );
+    )
   }
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 24,
-    backgroundColor: "#eaeaea",
-    marginTop: 32
-  },
-  title: {
-    marginTop: 16,
-    paddingVertical: 8,
-    backgroundColor: "#61dafb",
-    color: "#20232a",
-    textAlign: "center",
-    fontSize: 30,
-    fontWeight: "bold"
-  },
-  buttonField:{
-    flexDirection:'row',justifyContent: "center" 
-  }
-});
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+   }
+})
